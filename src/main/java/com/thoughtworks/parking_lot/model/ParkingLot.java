ef97a1172d6d75ce3794b75634eac58f2b47c681
murphylee2048 <1134697131@qq.com>
2019-07-18 17:06:05 +0800

    feat: create class ParkingLot

---
+++ src/main/java/com/thoughtworks/parking_lot/model/ParkingLot.java
@@ -0,0 +1,37 @@
+package com.thoughtworks.parking_lot.model;
+
+
+import javax.persistence.*;
+@Entity
+@Table(name="parkinglot")
+public class ParkingLot {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private String parkingLotName;
+    private int capacity;  // TODO: larger than 0
+    private String location;
+
+    public String getParkingLotName() {
+        return parkingLotName;
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setParkingLotName(String parkingLotName) {
+        this.parkingLotName = parkingLotName;
+    }
+
+    public void setCapacity(int capacity) {
+        this.capacity = capacity;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+}
